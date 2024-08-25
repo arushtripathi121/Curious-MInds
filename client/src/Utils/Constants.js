370686e7158c7f378ba0790cc5a6ecaf9f526301
arushtripathi121 <arushtripathi121@gamail.com>
2024-08-25 21:47:56 +0530

    commit

--- client/src/Utils/Constants.js
+++ client/src/Utils/Constants.js
@@ -1,3 +1,1 @@
-export const api2 = "https://server-ig01f3ppt-arushtripathi121s-projects.vercel.app/user/"
-
-export const api = "http://localhost:5000/Curious_Minds/api/v1/user/"
+export const api = "https://server-ig01f3ppt-arushtripathi121s-projects.vercel.app/Curious_Minds/api/v1/user/";
