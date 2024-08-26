ca9166c07aea1fb06ece37a732171073c2b292d5
arushtripathi121 <arushtripathi121@gamail.com>
2024-08-26 18:37:33 +0530

    server

--- Server/server.js
+++ Server/server.js
@@ -1,34 +1,39 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const app = express();
 const { connectToDb } = require('./Config/databse');
 require('dotenv').config();
 const router = require('./Router/router');
 
-const port = process.env.port;
+const port = process.env.PORT || 3000; // Default to 3000 if PORT is not defined
 
 const cloudinary = require('./Config/cloudinary');
 cloudinary.connectToCloudinary();
 
 const corsOptions = {
-    origin: process.env.frontendurl,
+    origin: process.env.FRONTEND_URL, // Use uppercase for environment variable names
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 200
 };
 
+// Apply CORS middleware globally
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// Connect to the database
 connectToDb();
 
+// Routes
 app.use("/Curious_Minds/api/v1/", router);
 
+// Root route
 app.get("/", (req, res) => {
     console.log("Server Started");
     res.send("Server is running!");
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`The app is running on port ${port}`);
 });
