c26bb156ea8d942f29ce4a7c9529e3d5a54edfaa
arushtripathi121 <arushtripathi121@gamail.com>
2024-08-26 19:06:50 +0530

    c

--- Server/server.js
+++ Server/server.js
@@ -9,15 +9,17 @@ const port = process.env.PORT || 3000;
 const cloudinary = require('./Config/cloudinary');
 cloudinary.connectToCloudinary();
 
-// Middleware to set custom headers
+// Move CORS middleware to the top
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        return res.status(200).json({});
-    }
-    next();
+  const allowedOrigins = 'https://client-sigma-drab.vercel.app/';  // Replace with your client origin
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
 });
 
 app.use(express.json());
@@ -27,9 +29,9 @@ connectToDb();
 app.use("/Curious_Minds/api/v1/", router);
 
 app.get("/", (req, res) => {
-    res.send("Server is running!");
+  res.send("Server is running!");
 });
 
 app.listen(port, () => {
-    console.log(`The app is running on port ${port}`);
+  console.log(`The app is running on port ${port}`);
 });
