a713a950b03c6b6bb15eb8e20a26a6c7073eac89
arushtripathi121 <arushtripathi121@gamail.com>
2024-08-26 18:56:29 +0530

    c

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const { connectToDb } = require('./Config/databse');
 require('dotenv').config();
@@ -10,30 +9,27 @@ const port = process.env.PORT || 3000;
 const cloudinary = require('./Config/cloudinary');
 cloudinary.connectToCloudinary();
 
-const corsOptions = {
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 200
-};
+// Middleware to set custom headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
 
-// Apply CORS middleware globally
-app.use(cors(corsOptions));
 app.use(express.json());
 
-// Connect to the database
 connectToDb();
 
-// Routes
 app.use("/Curious_Minds/api/v1/", router);
 
-// Root route
 app.get("/", (req, res) => {
-    console.log("Server Started");
     res.send("Server is running!");
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`The app is running on port ${port}`);
 });
